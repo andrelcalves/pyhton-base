df17b4e3b68ae8f738216d69e542d8649d7ce558
Andre Luis Carvalho Alves <andrealves@N093202.local>
2021-10-03 03:04:44 -0300

    add loop com quantidade de tentativas

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,24 +3,30 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+numero_rodada  = 1
+numero_tentativas = 3
 
 # função input é usada para obter um valor externo com interação no console, o input sempre devolve uma string
 # por isso será necessário converter com a função int para compara inteiro
 
-palpite = int(input("Digite o seu número: "))
-print("Você digitou", palpite)
+while(numero_rodada <= numero_tentativas):
+    print("------------------------------------")
+    print("Você está na rodada", numero_rodada, "de", numero_tentativas)
+    palpite = int(input("Digite o seu número: "))
+    print("Você digitou", palpite)
 
-acertou         = palpite == numero_secreto
-palpite_maior   = palpite > numero_secreto
-palpite_menor   = palpite < numero_secreto
+    acertou         = palpite == numero_secreto
+    palpite_maior   = palpite > numero_secreto
+    palpite_menor   = palpite < numero_secreto
 
-if(acertou):
-    print("Você acertou")
-else:
-    if(palpite_maior):
-        print("Você erro o número, seu palpite foi maior que o número secreto")
-    elif(palpite):
-        print("Vocë erro o número, seu palpite foi menor que o número secreto")
+    if(acertou):
+        print("Você acertou")
+    else:
+        if(palpite_maior):
+            print("Você erro o número, seu palpite foi maior que o número secreto")
+        elif(palpite):
+            print("Vocë erro o número, seu palpite foi menor que o número secreto")
+    numero_rodada = numero_rodada + 1
 
 print("Fim do jogo!")
 
